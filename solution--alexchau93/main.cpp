577850e2326b4a875e330117890d9c2d1a1f75f9
alexchau93 <chauder_head@yahoo.com>
2016-02-25 14:32:22 -0800

    finished assignment #2

---
+++ solution--alexchau93/main.cpp
@@ -0,0 +1,6 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Alex Chau <chauder_head@yahoo.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+I completed Assignment #2 with Steven Kha, Ryan Allen, and Arshi Singh.
